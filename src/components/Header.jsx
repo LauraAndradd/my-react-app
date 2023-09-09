2ad6a519b1b50d99ee0e918cd82329196d2a243a
Laura Andrade <lauraandrad05@gmail.com>
2023-09-09 16:58:07 -0300

    criar novos boards

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,8 @@ import AddEditBoardModal from '../modals/AddEditBoardModal';
 
 function Header({setBoardModalOpen, boardModalOpen}) {
 
-    const [openDropdown, setOpenDropdown] = useState(false);
+    const [openDropdown, setOpenDropdown] = useState(false)
+    const [boadType , setBoadType ] = useState('add')
 
     return (
         <div className=" p-4 fixed left-0 bg-white dark:bg-[#2b2c37] z-50 right-0 ">
@@ -17,14 +18,14 @@ function Header({setBoardModalOpen, boardModalOpen}) {
             <div className=" flex items-center space-x-2  md:space-x-4">
                 <img src={Logo} alt=" Logo " className=" h-6 w-6" />
                 <h3 className=" md:text-4xl  hidden md:inline-block font-bold  font-sans">
-                kanban
+                    Kanban
                 </h3>
-                <div>
+                <div className='flex items-center'>
                 <h3 className=" truncate max-w-[200px] md:text-2xl text-xl font-bold md:ml-20 font-sans  ">
                     board name
                 </h3>
                 <img src={openDropdown ? iconUp : iconDown} alt=" dropdown icon" className=" w-3 ml-2 cursor-pointer md:hidden"
-              onClick={() => setOpenDropdown(state => !state)}/>
+                onClick={() => setOpenDropdown(state => !state)}/>
                 </div>
             </div>
 
@@ -45,7 +46,7 @@ function Header({setBoardModalOpen, boardModalOpen}) {
 
             {openDropdown && <HeaderDropdown setBoardModalOpen={setBoardModalOpen} setOpenDropdown={setOpenDropdown}/>}
 
-            { boardModalOpen && <AddEditBoardModal setBoardModalOpen = {setBoardModalOpen}/> }
+            { boardModalOpen && <AddEditBoardModal type={boadType} setBoardModalOpen = {setBoardModalOpen}/> }
       
         </div>
 
